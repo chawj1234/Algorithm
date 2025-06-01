225cb86f3271a9e07b17ed6626a912c77c07ba3b
wonCha25 <84870285+chawj1234@users.noreply.github.com>
2025-06-01 14:58:37 +0900

    [level 0] Title: 원하는 문자열 찾기, Time: 1.73 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.swift
+++ 프로그래머스/0/181878. 원하는 문자열 찾기/원하는 문자열 찾기.swift
@@ -1,8 +1,5 @@
 import Foundation
 
 func solution(_ myString:String, _ pat:String) -> Int {
-    var myString = myString.lowercased()
-    var pat = pat.lowercased()
-    
-    return myString.contains(pat) ? 1 : 0
+    return myString.lowercased().contains(pat.lowercased()) ? 1 : 0
 }
